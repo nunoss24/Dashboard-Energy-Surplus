270731f3fd06bb1344aa4ae6be51b4b9b97e8541
nunoss24 <142089162+nunoss24@users.noreply.github.com>
2023-11-20 09:53:52 +0000

    Update database.js

--- servidor/src/models/database.js
+++ servidor/src/models/database.js
@@ -1,9 +1,9 @@
 var Sequelize = require('sequelize');
 
 const sequelize = new Sequelize(
-    'bd_projeto',
+    '',
     'postgres',
-    'qwerty2424',
+    '',
     {
         host: 'localhost',
         port: '5432',
